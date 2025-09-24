0b10960e52aa52ae58d7e7e977b11cfe9db8f286
Advay Parab <addyparab13@gmail.com>
2025-09-24 18:04:10 +0530

    server.py

--- server.py
+++ server.py
@@ -5,11 +5,13 @@ It handles API requests and serves responses.
 from fastmcp import FastMCP
 from tools.create_ticket import create_ticket
 from tools.update_ticket import update_ticket
+from tools.get_ticket import get_ticket
 
 app = FastMCP("zendesk-server")
 
 app.tool(description="Create a new Zendesk ticket")(create_ticket)
 app.tool(description="Update an existing Zendesk ticket")(update_ticket)
+app.tool(description="Get all ticket")(get_ticket)
 
 if __name__ == "__main__":
     print("🚀 FastMCP Zendesk Server started at http://127.1.0.1:8000/mcp")
