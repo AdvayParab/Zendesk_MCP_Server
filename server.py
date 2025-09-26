c89830e8061daad8551046785dd6ce6ef29f371c
Advay Parab <addyparab13@gmail.com>
2025-09-26 18:49:44 +0530

    server.py

--- server.py
+++ server.py
@@ -6,12 +6,24 @@ from fastmcp import FastMCP
 from tools.create_ticket import create_ticket
 from tools.update_ticket import update_ticket
 from tools.get_ticket import get_ticket
+from tools.add_comment import add_comment
+from tools.delete_ticket import delete_ticket
+from tools.get_single_ticket import get_ticket_by_id
+from tools.search_ticket import search_ticket
+from tools.list_users import list_users
+
 
 app = FastMCP("zendesk-server")
 
 app.tool(description="Create a new Zendesk ticket")(create_ticket)
 app.tool(description="Update an existing Zendesk ticket")(update_ticket)
 app.tool(description="Get all ticket")(get_ticket)
+app.tool(description="Add a comment to a Zendesk ticket")(add_comment)
+app.tool(description="Delete a Zendesk ticket")(delete_ticket)
+app.tool(description="Get a single Zendesk ticket by ID")(get_ticket_by_id)
+app.tool(description="Search Zendesk tickets")(search_ticket)
+app.tool(description="List all users in Zendesk")(list_users)
+
 
 if __name__ == "__main__":
     print("🚀 FastMCP Zendesk Server started at http://127.1.0.1:8000/mcp")
